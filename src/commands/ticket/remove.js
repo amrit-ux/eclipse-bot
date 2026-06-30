5dbbebe06110afcc372f78df1370455a5c13905e
amrit-ux <amritraj123000@gmail.com>
2026-06-30 12:25:15 +0530

    Update remove.js

--- src/commands/ticket/remove.js
+++ src/commands/ticket/remove.js
@@ -1,1 +1,17 @@
-// your code here
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("remove")
+    .setDescription("Remove user from ticket")
+    .addUserOption(opt =>
+      opt.setName("user").setDescription("User").setRequired(true)
+    ),
+
+  async execute(interaction) {
+    const user = interaction.options.getUser("user");
+    await interaction.channel.permissionOverwrites.delete(user.id);
+
+    await interaction.reply(`Removed ${user}`);
+  },
+};
