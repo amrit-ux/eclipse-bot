94191dceb57f2f31ac2c62e6569025feb879133d
amrit-ux <amritraj123000@gmail.com>
2026-06-30 12:24:24 +0530

    Implement rename command for ticket management

--- src/commands/ticket/rename.js
+++ src/commands/ticket/rename.js
@@ -1,1 +1,16 @@
-// your code here
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("rename")
+    .setDescription("Rename ticket")
+    .addStringOption(opt =>
+      opt.setName("name").setDescription("New name").setRequired(true)
+    ),
+
+  async execute(interaction) {
+    const name = interaction.options.getString("name");
+    await interaction.channel.setName(name);
+    await interaction.reply(`Renamed to ${name}`);
+  },
+};
