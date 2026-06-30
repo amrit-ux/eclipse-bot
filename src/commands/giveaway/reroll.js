0183f679d436819a038cfb8e75c7a2529a8edf02
amrit-ux <amritraj123000@gmail.com>
2026-06-30 12:26:35 +0530

    Implement reroll command for giveaways

--- src/commands/giveaway/reroll.js
+++ src/commands/giveaway/reroll.js
@@ -1,1 +1,11 @@
-// your code here
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("reroll")
+    .setDescription("Reroll giveaway"),
+
+  async execute(interaction) {
+    await interaction.reply("🔄 Rerolled");
+  },
+};
