a6c13d788d5d5ca27f7ba7d901c04058acae7d71
amrit-ux <amritraj123000@gmail.com>
2026-06-30 12:26:18 +0530

    Implement end giveaway command

--- src/commands/giveaway/end.js
+++ src/commands/giveaway/end.js
@@ -1,1 +1,11 @@
-// your code here
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("gend")
+    .setDescription("End giveaway"),
+
+  async execute(interaction) {
+    await interaction.reply("Giveaway ended");
+  },
+};
