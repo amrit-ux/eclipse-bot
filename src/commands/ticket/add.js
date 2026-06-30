8ffbfa1f751aa8efe3d05a75c42972d671c90f6d
amrit-ux <amritraj123000@gmail.com>
2026-06-30 12:24:55 +0530

    Update add.js

--- src/commands/ticket/add.js
+++ src/commands/ticket/add.js
@@ -1,1 +1,19 @@
-// your code here
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("add")
+    .setDescription("Add user to ticket")
+    .addUserOption(opt =>
+      opt.setName("user").setDescription("User").setRequired(true)
+    ),
+
+  async execute(interaction) {
+    const user = interaction.options.getUser("user");
+    await interaction.channel.permissionOverwrites.create(user.id, {
+      ViewChannel: true,
+    });
+
+    await interaction.reply(`Added ${user}`);
+  },
+};
