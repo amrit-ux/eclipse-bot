d171f5e7c5d50337997bb51f81226c20d04e41fd
amrit-ux <amritraj123000@gmail.com>
2026-06-30 12:23:50 +0530

    Add close command to manage tickets
    Implemented close command for ticket management.

--- src/commands/ticket/close.js
+++ src/commands/ticket/close.js
@@ -1,1 +1,12 @@
-// your code here
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("close")
+    .setDescription("Close ticket"),
+
+  async execute(interaction) {
+    await interaction.reply("Closing ticket...");
+    setTimeout(() => interaction.channel.delete(), 2000);
+  },
+};
