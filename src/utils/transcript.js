8513bdec332233a27b64a75d2dde2b1ba7b69a74
amrit-ux <amritraj123000@gmail.com>
2026-06-30 11:58:53 +0530

    Add createTranscript function to generate HTML transcripts
    Implement createTranscript function to fetch and save channel messages as HTML.

--- src/utils/transcript.js
+++ src/utils/transcript.js
@@ -1,1 +1,20 @@
-// your code here
+const fs = require("fs");
+
+async function createTranscript(channel) {
+  const messages = await channel.messages.fetch({ limit: 100 });
+
+  let html = `<html><body><h2>Transcript</h2>`;
+
+  messages.reverse().forEach(msg => {
+    html += `<p><b>${msg.author.tag}:</b> ${msg.content}</p>`;
+  });
+
+  html += "</body></html>";
+
+  const file = `./transcript-${channel.id}.html`;
+  fs.writeFileSync(file, html);
+
+  return file;
+}
+
+module.exports = { createTranscript };
