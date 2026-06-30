0e67f9031db0cced58ee789d747ebe519816db0f
amrit-ux <amritraj123000@gmail.com>
2026-06-30 11:56:16 +0530

    Implement command handler to load commands dynamically

--- src/handlers/commandHandler.js
+++ src/handlers/commandHandler.js
@@ -1,1 +1,14 @@
-// your code here
+const fs = require("fs");
+
+module.exports = (client) => {
+  const folders = fs.readdirSync("./src/commands");
+
+  for (const folder of folders) {
+    const files = fs.readdirSync(`./src/commands/${folder}`).filter(f => f.endsWith(".js"));
+
+    for (const file of files) {
+      const command = require(`../commands/${folder}/${file}`);
+      client.commands.set(command.data.name, command);
+    }
+  }
+};
